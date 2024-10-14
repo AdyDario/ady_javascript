5f1d281d1db2320c6795ce258d42e047a070599e
Adilson Dario <104008714+AdyDario@users.noreply.github.com>
2024-10-14 14:14:45 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+
+var nome = prompt ("insira o seu nome");
+var sobrenome = prompt ("insira seu sobrenome");
+var nomeSobrenome = nome + sobrenome;
+    alert("olá visitante") 
+    console.log(nomeSobrenome);
+
+var idade = prompt("insira sua idade");
+var anoAtual = prompt("insira o ano atual");
+var anoNascimento = anoAtual - idade;
+    console.log(anoNascimento);
+
+    var texto = prompt ("Qual seu sorvete preferido?");
+    var texto2 = prompt ("Qual a sua calda de sorvete preferida?");
+    var texto3 = texto + texto2; 
+        console.log(texto3);
